318e3eb219ff43a2e067be1fc058dfa51d256ec0
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-06-27 15:37:04 +0100

    Including Nav_Bar in column

--- lib/screens/Home/home_page.dart
+++ lib/screens/Home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Components/app_bar.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -12,10 +14,14 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-        height: size.height,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/images/bkg.jpg'), fit: BoxFit.cover),
-        ));
+      height: size.height,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/images/bkg.jpg'), fit: BoxFit.cover),
+      ),
+      child: const Column(children: [
+        CustomAppBar(),
+      ]),
+    );
   }
 }
